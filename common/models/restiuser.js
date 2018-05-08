589634ea3ea7415773a44283ffa41898d7103f49
DESKTOP-6U8JP9R\Ricky <rickyhermawan03@gmail.com>
2018-05-09 02:25:37 +0700

    create api mahasiswa dan dosen

--- common/models/restiuser.js
+++ common/models/restiuser.js
@@ -1,5 +1,113 @@
 'use strict';
 
-module.exports = function(Restiuser) {
+module.exports = function (Restiuser) {
+    let request = require('request');
+    let app = require('../../server/server');
+
+    /**
+     * Create Mahasiswa Logic
+     */
+
+    Restiuser.remoteMethod(
+        'createMahasiswa', {
+            accepts: [{
+                arg: 'params',
+                type: 'Object',
+                required: true,
+                http: { source: 'body' }
+            }, {
+                arg: "options",
+                type: "object",
+                http: "optionsFromRequest"
+            }],
+            returns: {
+                arg: 'createMahasiswa', type: 'array', root: true
+            },
+            http: {
+                path: '/createMahasiswa',
+                verb: 'post'
+            }
+        });
+
+    Restiuser.createMahasiswa = function (params, options, cb) {
+        console.log(params, 'Params');
+        Restiuser.create(params, function (error, data) {
+            console.log(data);
+            if (error) {
+                cb(error);
+                console.log(error.statusCode, 'Errornya')
+            } else {
+                let mahasiswa = app.models.Mahasiswa;
+                const dataMahasiswa = {
+                    'username': params.username,
+                    'nim': params.nim,
+                    'jurusan': params.jurusan
+                }
+                mahasiswa.create(dataMahasiswa, function (error, data) {
+                    console.log(data);
+                    if (error) {
+                        cb(error);
+                        console.log(error.statusCode, 'Errornya')
+                    } else {
+                        cb(error, data);
+                    }
+                });
+            }
+        });
+    }
+
+    /**
+     * Create Dosen Logic
+     */
+    
+    Restiuser.remoteMethod(
+        'createDosen', {
+            accepts: [{
+                arg: 'params',
+                type: 'Object',
+                required: true,
+                http: { source: 'body' }
+            }, {
+                arg: "options",
+                type: "object",
+                http: "optionsFromRequest"
+            }],
+            returns: {
+                arg: 'createDosen', type: 'array', root: true
+            },
+            http: {
+                path: '/createDosen',
+                verb: 'post'
+            }
+        });
+
+    Restiuser.createDosen = function (params, options, cb) {
+        console.log(params, 'Params');
+        Restiuser.create(params, function (error, data) {
+            console.log(data);
+            if (error) {
+                cb(error);
+                console.log(error.statusCode, 'Errornya')
+            } else {
+                let dosen = app.models.Dosen;
+                const dataDosen = {
+                    'username': params.username,
+                    'nip': params.nip,
+                    'status': params.status,
+                    'jurusan': params.jurusan
+                }
+                dosen.create(dataDosen, function (error, data) {
+                    console.log(data);
+                    if (error) {
+                        cb(error);
+                        console.log(error.statusCode, 'Errornya')
+                    } else {
+                        cb(error, data);
+                    }
+                });
+            }
+        });
+    }
+
 
 };
